38da8e8bf9f4a2d03327f02a6a84414f6fa90b60
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-10-02 21:04:08 -0500

    Initialized FEM project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import JobListing from "./JobListing";
+
+function App() {
+  return (
+    <>
+      <h1>Good Evening</h1>
+      <JobListing></JobListing>
+    </>
+  );
+}
+
+export default App;
