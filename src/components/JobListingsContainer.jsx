945faa43acd7e3a282990e93d9e04cb1678bb096
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-11-30 12:22:35 -0600

    Fixed CSS layout and animations

--- src/components/JobListingsContainer.jsx
+++ src/components/JobListingsContainer.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FilterListContext } from "../context/FilterListProvider.jsx";
 import { CurrentJobsContext } from "../context/CurrentJobsProvider.jsx";
 import { CurrentJobSelecedContext } from "../context/CurrentJobSelectedProvider.jsx";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { filterJobs } from "../utilities.js";
 import JobListing from "./JobListing.jsx";
 import SelectedJobDetails from "./SelectedJobDetails.jsx";
 import data from "../../data.json";
+import { Drawer } from "@mui/material";
 
 data.forEach((item) => {
   item.features = [item.role, item.level, ...item.languages, ...item.tools];
@@ -16,27 +18,62 @@ currentJobList.forEach((item) => (item.show = true));
 function JobListingsContainer() {
   const { filterList } = useContext(FilterListContext);
   const { currentJobs, setCurrentJobs } = useContext(CurrentJobsContext);
-  const { currentJobSelected } = useContext(CurrentJobSelecedContext);
+  const { currentJobSelected, setCurrentJobSelected } = useContext(
+    CurrentJobSelecedContext
+  );
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     setCurrentJobs(filterJobs(filterList));
   }, [filterList, setCurrentJobs]);
 
+  useEffect(() => {
+    if (currentJobSelected != undefined) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [currentJobSelected]);
+
   return (
     <>
-      <div>
-        {currentJobs.map((job) => {
-          if (job.show == true) {
-            return (
-              <JobListing key={job.id} job={job}>
-                {" "}
-              </JobListing>
-            );
-          }
-        })}
-      </div>
-      {currentJobSelected != undefined && (
-        <SelectedJobDetails job={currentJobSelected}></SelectedJobDetails>
+      {/* <div style={{ width: "100%" }}> */}
+      {currentJobs.map((job) => {
+        if (job.show == true) {
+          return (
+            <JobListing key={job.id} job={job}>
+              {" "}
+            </JobListing>
+          );
+        }
+      })}
+      {/* </div> */}
+      {open && (
+        <Drawer
+          open={open}
+          anchor="right"
+          variant="persistent"
+          sx={{
+            width: "350px",
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              backgroundColor: "#242424;",
+              color: "white",
+              width: "350px",
+              boxSizing: "border-box",
+              border: "2px solid rgb(160, 200, 250)",
+              borderRight: "0px solid",
+              borderRadius: "10px",
+              padding: "20px",
+            },
+          }}
+          onClose={() => setCurrentJobSelected()}
+        >
+          <SelectedJobDetails
+            job={currentJobSelected}
+            setOpen={setOpen}
+          ></SelectedJobDetails>
+        </Drawer>
       )}
     </>
   );
--- src/components/SelectedJobDetails.jsx
+++ src/components/SelectedJobDetails.jsx
@@ -3,13 +3,15 @@ import "../css/selectedJob.css";
 import { X } from "lucide-react";
 import { useContext } from "react";
 import { CurrentJobSelecedContext } from "../context/CurrentJobSelectedProvider";
-function SelectedJobDetails({ job }) {
+
+function SelectedJobDetails({ job, setOpen }) {
   const { setCurrentJobSelected } = useContext(CurrentJobSelecedContext);
   function handleCloseDetails() {
     setCurrentJobSelected();
+    setOpen(false);
   }
   return (
-    <div className="selected-job-container">
+    <>
       <X className="close-job-details" onClick={handleCloseDetails} />
       <div className="main-info">
         <h2>{job.position}</h2>
@@ -20,9 +22,14 @@ function SelectedJobDetails({ job }) {
           <span>·</span>
           <p>{job.location}</p>
         </div>
-        <p className="job-description"> {job.description} </p>
       </div>
-    </div>
+      <p className="job-description"> {job.description} </p>
+      <button className="scale-button">
+        {" "}
+        <span className="scale-text">Apply Now</span>
+        <span className="scale-bg"></span>
+      </button>
+    </>
   );
 }
 
